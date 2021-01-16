c57452af8f2ba4dd2d6d0fd266e633f1fbea0ad3
denis200210 <71909269+denis200210@users.noreply.github.com>
2021-01-16 21:42:11 +0900

    Delete Function.h

--- Function.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by 79146 on 15.01.2021.
-//
-
-#ifndef UNTITLED9_FUNCTION_H
-#define UNTITLED9_FUNCTION_H
-void MakeTerm(string *Term,int n,string s,int *k);
-#endif //UNTITLED9_FUNCTION_H
